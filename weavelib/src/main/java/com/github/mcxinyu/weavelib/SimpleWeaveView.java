9433f13f626ed71fe9ddc0566089c23a23eae3b3
huangyuefeng <mcxinyu@foxmail.com>
2019-04-03 10:27:51 +0800

    Initial commit

---
+++ weavelib/src/main/java/com/github/mcxinyu/weavelib/SimpleWeaveView.java
@@ -0,0 +1,27 @@
+package com.github.mcxinyu.weavelib;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * 简单的单线条编织 view
+ * <p>
+ * Created by huangyuefeng on 2019/4/3.
+ * Contact me : mcxinyu@foxmail.com
+ */
+public class SimpleWeaveView extends View {
+
+    public SimpleWeaveView(Context context) {
+        super(context);
+    }
+
+    public SimpleWeaveView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SimpleWeaveView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
